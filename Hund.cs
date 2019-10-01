45d56de439fd69e57849aa86dbbaf50fbd92fdad
Fille <felipe.vilca87@gmail.com>
2019-10-01 13:46:33 +0200

    class

---
+++ Hund.cs
@@ -0,0 +1,49 @@
+ class Dog
+ {
+        string name;
+        string race;
+        int age;
+       
+       
+        public string Name
+       {
+              get
+              {
+                     return name;
+              }
+              set
+              {
+                     name = value;
+              }
+        
+       }
+       public string Race
+       {
+              get
+              {
+                     return race;
+              }
+              set
+              {
+                     race = value;
+              }
+
+       }
+       public int Age
+       {
+              get
+              {
+                     return age;
+              }
+              set
+              {
+                     age = value;
+              }
+       }
+      
+       
+
+ }
+ 
+ 
+ 
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace excercises_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Skapa en konstruktor till din djur-klass som tar emot värden för namn, 
+            ålder, ras eller vad du nu skapat för variabler. Använd konstruktorn i ett testobjekt.
+            */
+            Dog myDog = new Dog();
+            
+            Console.WriteLine("Hej vad har du för djur? ");
+            string myDogName = Console.ReadLine();
+            Console.WriteLine("Hur gammal är din " + myDogName + " ?");
+            int myDogAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Vad är " + myDogName + " för ras? ");
+            string myDogRace = Console.ReadLine();
+            
+            Console.WriteLine("Ditt favorit djur är " + myDogName + 
+            " och är av rasen " + myDogRace + " och är " + myDogAge + " år gammal");
+        }
+    }
+}
